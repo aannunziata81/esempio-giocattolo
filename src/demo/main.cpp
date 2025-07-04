fb2a62fc5514a7a61a305cf2d6e57f9f9c3bb8bb
aannunziata81 <supe_rtony@hotmail.it>
2025-07-04 10:34:15 +0200

    Add sum function

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+void sumTwoElement(const int &element1, const int &element2, int &total)
+{
+  total = element1 + element2;
+}
+
 int main(int argc, char const *argv[])
 {
   std::cout << "Hello example" << std::endl;
+  constexpr int element1{2};
+  constexpr int element2{54};
+  int total{};
+  sumTwoElement(element1, element2, total);
+  std::cout << "Sum: " << total << std::endl;
   return EXIT_SUCCESS;
 }
