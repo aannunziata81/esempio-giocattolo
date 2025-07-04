7f84a27f9dbe7e7d26d34ad047533a055b2d7b8d
Lextyle92 <lextyle.92@gmail.com>
2025-07-04 11:14:14 +0200

    Add subdirectory to CMakeLists.txt,  Link Point2D library to main, Add Point2D test in main.

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "Point2D.h"
 
 int main(int argc, char const *argv[])
 {
   std::cout << "Hello example" << std::endl;
+
+  Point2D p1 = Point2D();
+  std::cout << p1 << std::endl;
+  p1 = Point2D(4, 7);
+  std::cout << p1 << std::endl;
+
   return EXIT_SUCCESS;
 }
