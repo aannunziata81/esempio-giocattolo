11cab892412c2c59d6987156010605ceb60e6f50
aannunziata81 <supe_rtony@hotmail.it>
2025-07-04 10:48:44 +0200

    Modify names of added functions

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-void sumTwoElement(const int &element1, const int &element2, int &total)
+void sumTwoElements(const int &element1, const int &element2, int &total)
 {
   total = element1 + element2;
 }
 
-void subTwoElement(const int &element1, const int &element2, int &total)
+void subtractionTwoElements(const int &element1, const int &element2, int &total)
 {
   total = element1 - element2;
 }
@@ -16,9 +16,9 @@ int main(int argc, char const *argv[])
   constexpr int element1{2};
   constexpr int element2{54};
   int total{};
-  sumTwoElement(element1, element2, total);
+  sumTwoElements(element1, element2, total);
   std::cout << "Sum: " << total << std::endl;
-  subTwoElement(element2, element1, total);
+  subtractionTwoElements(element2, element1, total);
   std::cout << "Subtraction: " << total << std::endl;
   return EXIT_SUCCESS;
 }
