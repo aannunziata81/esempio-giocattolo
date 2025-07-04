ec3dea8fe1005e7db9c2036145196591c5898b7a
aannunziata81 <supe_rtony@hotmail.it>
2025-07-04 10:45:42 +0200

    Add subtraction function

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -5,6 +5,11 @@ void sumTwoElement(const int &element1, const int &element2, int &total)
   total = element1 + element2;
 }
 
+void subTwoElement(const int &element1, const int &element2, int &total)
+{
+  total = element1 - element2;
+}
+
 int main(int argc, char const *argv[])
 {
   std::cout << "Hello example" << std::endl;
@@ -13,5 +18,7 @@ int main(int argc, char const *argv[])
   int total{};
   sumTwoElement(element1, element2, total);
   std::cout << "Sum: " << total << std::endl;
+  subTwoElement(element2, element1, total);
+  std::cout << "Subtraction: " << total << std::endl;
   return EXIT_SUCCESS;
 }
