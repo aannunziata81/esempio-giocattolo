0497950447802b634f04ba07e4974db2ed3ef345
aannunziata81 <63741163+aannunziata81@users.noreply.github.com>
2025-07-04 13:27:08 +0200

    New features from task-1 branch
    The commits on the task-1 branch are as follows:
    
    * Add sum function
    
    * Add subtraction function
    
    * Modify names of added functions
    
    * Remove const &

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -2,9 +2,27 @@
 #include <Point2D.h>
 #include <Point3D.h>
 
+void sumTwoElements(int element1, int element2, int &total)
+{
+  total = element1 + element2;
+}
+
+void subtractionTwoElements(int element1, int element2, int &total)
+{
+  total = element1 - element2;
+}
+
+
 int main(int argc, char const *argv[])
 {
   std::cout << "Hello example" << std::endl;
+  constexpr int element1{2};
+  constexpr int element2{54};
+  int total{};
+  sumTwoElements(element1, element2, total);
+  std::cout << "Sum: " << total << std::endl;
+  subtractionTwoElements(element2, element1, total);
+  std::cout << "Subtraction: " << total << std::endl;
 
   Point2D p1 = Point2D();
   std::cout << p1 << std::endl;
